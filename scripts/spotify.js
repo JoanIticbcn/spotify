76faf8105b15aa941cd505af2f09b0acfb65b859
Joan_Montanya <2023_joan.montanya@iticbnc.cat>
2024-12-09 20:44:32 +0100

    AccesToken

---
+++ scripts/spotify.js
@@ -0,0 +1,44 @@
+import { clientId,clientSecret } from "../env/client.js";
+
+const inputsong = document.getElementById("inputsong")
+const btnBuscar = document.getElementById("btnbuscar")
+const btnBorrar = document.getElementById("btnborrar")
+btnBuscar.disabled = true;
+btnBorrar.disabled = true;
+let tokenacces;
+
+const getSpotifyAccessToken = function (clientId, clientSecret) {
+    // Url de l'endpont de spotify
+    const url = "https://accounts.spotify.com/api/token";
+    // ClientId i ClienSecret generat en la plataforma de spotify
+    const credentials = btoa(`${clientId}:${clientSecret}`);
+    //Es crear un header on se li passa les credencials
+    const header = {
+      Authorization: `Basic ${credentials}`,
+      "Content-Type": "application/x-www-form-urlencoded",
+    };
+    fetch(url, {
+      method: "POST",
+      headers: header,
+      body: "grant_type=client_credentials", // Paràmetres del cos de la sol·licitud
+    })
+      .then((response) => {
+        // Controlar si la petició ha anat bé o hi ha alguna error.
+        if (!response.ok) {
+          throw new Error(`Error: ${response.status} - ${response.statusText}`);
+        }
+        return response.json(); // Retorna la resposta com JSON
+      })
+      .then((data) => {
+        // Al data retorna el token d'accés que necessitarem
+        // Haurem d’habilitar els botons “Buscar” i “Borrar”
+        tokenacces = data.acces_token
+        btnBuscar.disabled = false
+        btnBorrar.disabled = false
+      })
+      .catch((error) => {
+        // SI durant el fetch hi ha hagut algun error arribarem aquí.
+        console.error("Error a l'obtenir el token:", error);
+      });
+    };
+getSpotifyAccessToken(clientId,clientSecret)
